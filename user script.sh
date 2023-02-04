c87934b9c8c644ce0c19b04ac9f6384c825843f9
Moosenensen <hunter@franklinhome.net.au>
2023-02-04 15:20:07 +1000

    fixed if command not working

--- user script.sh
+++ user script.sh
@@ -1,12 +1,16 @@
+rm cuser.txt
+getent passwd | awk -F: '$3 >= 1000 {print $1}' >> cuser.txt
+
 rm diff2
 diff cuser.txt users.txt | grep '^[>]' >> diff2
+rm newusers
 sed 's/> //g' diff2 >> newusers
 rm diff2
+
 for i in $( cat newusers ); do
 useradd $i
 echo $i:CyberTaipan123! | chpasswd
 done
-
 
 rm diff3
 diff cuser.txt users.txt | grep '^[<]' >> diff3
@@ -16,10 +20,10 @@ cat badusers
 rm diff3
 echo delete all? y/n
 read delete
-if [ "y" == "$delete" ]
+if [ "$y" = "$delete" ]
 then
 
-	for i in $( cat badusers ); do
+for i in $( cat badusers ); do
 sudo userdel $i
 	done
 fi
