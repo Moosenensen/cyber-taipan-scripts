27a10df51a6b974a25cc8e4484438c29082e3e62
Moosenensen <hunter@franklinhome.net.au>
2023-02-04 18:29:32 +1000

    the bash stands for bash my head into the wall

--- user script.sh
+++ user script.sh
@@ -1,13 +1,14 @@
 # the removes are to make sure the file doesnt alreeady exist
 rm cuser.txt
+rm users.txt
 #gets current users
 getent passwd | awk -F: '$3 >= 1000 {print $1}' >> cuser.txt
-sed 's/ //g' users.txt >> users.txt
+sed 's/ //g' wusers.txt >> users.txt
 # finds the users that are in users.txt but not in current users
 rm diff2
 diff cuser.txt users.txt | grep '^[>]' >> diff2
 rm newusers
-sed 's/> //g' diff2 > newusers
+sed 's/> //g' diff2 >> newusers
 rm diff2
 #loops through and adds a user with the content of every line
 for i in $( cat newusers ); do
@@ -15,6 +16,7 @@ useradd $i
 echo $i:CyberTaipan123! | chpasswd
 echo "added user $i"
 done
+
 
 #finds all the names that are in current users but not user.txt
 rm diff3
@@ -32,5 +34,6 @@ then
 for i in $( cat badusers ); do
 sudo userdel $i
 echo "deleted user $i"
-	done
+done
 fi
+
