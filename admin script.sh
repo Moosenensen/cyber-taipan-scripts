a5ca6d4671070956e1fceb384a9e919edaa719e9
Moosenensen <hunter@franklinhome.net.au>
2023-02-04 18:30:08 +1000

    ahhhhhhhhhhhhhhhhhhhhhhhhhhh

---
+++ admin script.sh
@@ -0,0 +1,20 @@
+rm cadmin
+getent group sudo | cut -d: -f4 >> cadmin
+rm admin2
+sed 's/ //g' admin >> admin2
+rm newadmin
+diff admin2 cadmin | grep '^[< ]' >> newadmin
+rm newadmin2
+sed 's/< //g' newadmin >> newadmin2
+for i in $( cat newadmin2 ); do
+sudo usermod -aG sudo $i
+echo added admin $i
+done
+rm badadmin
+rm badadmin2
+diff admin2 cadmin | grep '^[> ]' >> badadmin
+sed 's/> //g' badadmin >> badadmin2
+for i in $( cat badadmin2 ); do
+sudo deluser $i sudo
+done
+
