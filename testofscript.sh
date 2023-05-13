4ead4ddeba2fd127a8d773545563e76a73fb5a67
usernamegth <112678550+usernamegth@users.noreply.github.com>
2023-05-13 18:54:06 +1000

    testing a user adding script
    aaaaaaaaaaaaaaa

--- testofscript.sh
+++ testofscript.sh
@@ -5,6 +5,7 @@ rm test2.txt
 rm test3.txt
 sort users.txt > Susers.txt
 awk -F: '$3>=1000{print $1}' /etc/passwd > Lusers.txt
+sort Lusers.txt
 diff Susers.txt Lusers.txt > test.txt
 grep "^<" test.txt > test2.txt
 sed 's/<//g; s/ //g' test2.txt > test3.txt
