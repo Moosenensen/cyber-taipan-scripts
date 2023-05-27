083c318df763002a609a4027c3d63349eb8c4d7c
Moosenensen <hunter@franklinhome.net.au>
2023-05-27 17:19:44 +1000

    group add program yay

--- deletefiles.sh
+++ deletefiles.sh
@@ -1,1 +1,29 @@
-regex. posix- extend -regextype find command
+#regex. posix- extend -regextype find command
+
+#!/bin/bash
+
+cd /home
+# Search for audio files and store them in an array
+audio_files=($(find . -type f \( -name "*.mp3" -o -name "*.flac" -o -name "*.wav" -o -name "*.ogg" \)))
+
+# Check if any audio files were found
+if [[ ${#audio_files[@]} -eq 0 ]]; then
+  echo "No audio files found."
+  exit 0
+fi
+
+# Display the list of audio files
+echo "Found audio files:"
+for file in "${audio_files[@]}"; do
+  echo "$file"
+done
+
+# Ask for confirmation to delete files
+echo
+read -p "Do you want to delete these files" response
+
+if [[ "$response" == [y] ]]; then
+    for file in "${audio_files[@]}"; do
+  rm $file
+done
+fi
