7fa01718b601e34ca3c276a073592e0d04927bdf
Jacob_str <80145275+Ghdhdhdh@users.noreply.github.com>
2023-06-14 17:33:17 +1000

    Update deletefiles.sh
    Fixed Bug Error

--- deletefiles.sh
+++ deletefiles.sh
@@ -4,7 +4,7 @@
 
 cd /home
 # Search for audio files and store them in an array
-audio_files=($(find /home -type f \(-name "*.3gp" -o -name "*.aa" -o -name "*.aac" -o -name "*.aax" -o -name "*.act" -o -name "*.aiff" -o -name "*.alac" -o -name "*.amr" -o -name "*.ape" -o -name "*.au" -o -name "*.awb" -o -name "*.dss" -o -name "*.dvf" -o -name "*.flac" -o -name "*.gsm" -o -name "*.iklax" -o -name "*.ivs" -o -name "*.m4a" -o -name "*.m4b" -o -name "*.m4p" -o -name "*.mmf" -o -name "*.movpkg" -o -name "*.mp3" -o -name "*.mpc" -o -name "*.msv" -o -name "*.nmf" -o -name "*.ogg" -o -name "*.opus" -o -name "*.ra" -o -name "*.raw" -o -name "*.rf64" -o -name "*.sln" -o -name "*.tta" -o -name "*.voc" -o -name "*.vox" -o -name "*.wav" -o -name "*.wma" -o -name "*.wv" -o -name "*.webm" -o -name "*.8svx" -o -name "*.cda" -o -name "*.oga" -o -name "*.mogg" -o -name "*.rm" \)))
+audio_files=($(find /home -type f \( -name "*.3gp" -o -name "*.aa" -o -name "*.aac" -o -name "*.aax" -o -name "*.act" -o -name "*.aiff" -o -name "*.alac" -o -name "*.amr" -o -name "*.ape" -o -name "*.au" -o -name "*.awb" -o -name "*.dss" -o -name "*.dvf" -o -name "*.flac" -o -name "*.gsm" -o -name "*.iklax" -o -name "*.ivs" -o -name "*.m4a" -o -name "*.m4b" -o -name "*.m4p" -o -name "*.mmf" -o -name "*.movpkg" -o -name "*.mp3" -o -name "*.mpc" -o -name "*.msv" -o -name "*.nmf" -o -name "*.ogg" -o -name "*.opus" -o -name "*.ra" -o -name "*.raw" -o -name "*.rf64" -o -name "*.sln" -o -name "*.tta" -o -name "*.voc" -o -name "*.vox" -o -name "*.wav" -o -name "*.wma" -o -name "*.wv" -o -name "*.webm" -o -name "*.8svx" -o -name "*.cda" -o -name "*.oga" -o -name "*.mogg" -o -name "*.rm" \)))
 
 # Check if any audio files were found
 if [[ ${#audio_files[@]} -eq 0 ]]; then
