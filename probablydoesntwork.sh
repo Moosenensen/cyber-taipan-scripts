b79bd2a6c2c742b1e8387682c7415891adc64145
usernamegth <112678550+usernamegth@users.noreply.github.com>
2023-06-24 17:46:47 +1000

    Create probablydoesntwork.sh

---
+++ probablydoesntwork.sh
@@ -0,0 +1,6 @@
+find /home -regextype posix-extended -regex '.*\.(midi|mid|mod|mp3|mp2|mpa|abs|mpega|au|snd|wav|aiff|aif|sid|mkv|flac|ogg)$' >> soundfiles.txt
+find /home -regextype posix-extended -regex '.*\.(mpeg|mpg|mpe|dl|movie|movi|mv|iff|anim5|anim3|anim7|avi|vfw|avx|fli|flc|mov|qt|spl|swf|dcr|dir|dxr|rpm|rm|smi|ra|ram|rv|wmv|asf|asx|wma|wax|wmv|wmx|3gp|mov|mp4|flv|m4v|xlsx|pptx|docx|csv)$' >> videofiles.txt
+find /home -regextype posix-extended -regex '.*\.(tiff|tif|rs|iml|gif|jpeg|exe|torrent|pdf|run|bat|jpg|jpe|png|rgb|xwd|xpm|ppm|pbm|pgm|pcx|ico|svg|svgz|pot|xml|pl)$' >> imgfiles.txt
+grep -v "sci" soundfiles.txt >> soundfiles.txt 
+grep -v "sci" videofiles.txt >> videofiles.txt
+grep -v "sci" imgfiles.txt >> imgfiles.txt
