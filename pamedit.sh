3d9f8da7309c7561da409de7f7285e4982fe82fd
usernamegth <112678550+usernamegth@users.noreply.github.com>
2023-07-15 17:30:18 +1000

    Create pamedit.sh
    currently being worked on, dont rely on this until its finished

---
+++ pamedit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+# SECTION 0
+# CREATING BACKUPS
+mkdir backups
+# SECTION 0.1 
+# CHECKING THE DIRECTORY 
+current_directory=$(basename "$(pwd)")
+if ["$current_directory" == "Desktop"]
+# SECTION 1
+# CHANGES PASS_MAX_DAYS TO 30
+w="30";file="/etc/login.defs";f="7"
+l=$(grep -n "PASS_MAX_DAYS" "$file" | cut -d':' -f1 | tail -n 1)
+sed -i "${l}s/[0-9]\+/$w/" "$file"
+echo "PASS_MAX_DAYS changed to 30."
+# SECTION 2
+# CHANGES PASS_MIN_DAYS TO 7
+h=$(grep -n "PASS_MIN_DAYS" "$file" | cut -d':' -f1 | tail -n 1)
+sed -i "${h}s/[0-9]\+/$f/" "$file"
+echo "PASS_MIN_DAYS changed to 7."
