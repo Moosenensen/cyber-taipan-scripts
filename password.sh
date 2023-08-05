16950914a9b4ceb5c3c807c5bbd40b0b2a0c9c66
usernamegth <112678550+usernamegth@users.noreply.github.com>
2023-08-05 18:06:28 +1000

    Update password.sh

--- password.sh
+++ password.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-awk -F: '$3>=1000{print $1}' /etc/passwd >> current.txt
-for i in ($cat current.txt); do
-echo "$i:CyberTaipan123!" | sudo chpasswd
+current_user=$(whoami) # gets the user who is running the script, sets the variable current_user
+awk -F: '$3>=1000{print $1}' /etc/passwd >> current.txt # extracts all current users from the system, moves them to current.txt
+sed -i "/$current_user/d" current.txt # removes the current user from this file just in case
+for i in ($cat current.txt); do 
+echo "$i:CyberTaipan123!" | sudo chpasswd # changes password to 'CyberTaipan123!'
 done 
-# short script, doesn't user users.txt
+# short script, doesn't use users.txt
