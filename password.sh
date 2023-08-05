71ad25769049983084848bb0fd8c1d5ef8f5f354
usernamegth <112678550+usernamegth@users.noreply.github.com>
2023-08-05 17:57:01 +1000

    Update password.sh

--- password.sh
+++ password.sh
@@ -3,4 +3,4 @@ awk -F: '$3>=1000{print $1}' /etc/passwd >> current.txt
 for i in ($cat current.txt); do
 echo "$i:CyberTaipan123!" | sudo chpasswd
 done 
-# short script
+# short script, doesn't user users.txt
