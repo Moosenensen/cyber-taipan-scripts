1cbf548b0460f376dc8b48f11ea2a8b88801b26b
Riley <111324867+The3agle@users.noreply.github.com>
2023-10-07 17:49:06 +1030

    Update newCrontab.sh

--- newCrontab.sh
+++ newCrontab.sh
@@ -1,7 +1,7 @@
 for user in $(getent passwd | cut -f1 -d: )
 do
 
-crontabresults=crontab $user -l
+crontabresults=crontab -u $user -l
 if [[ $crontabresults =! "no crontab for $user"]]
 echo $user
 echo $crontabresults
