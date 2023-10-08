6dca1b90b0816e4d5da2fec706ea554d5a4849fa
usernamegth <112678550+usernamegth@users.noreply.github.com>
2023-10-08 15:10:23 +1100

    Update netcatscan.sh

--- untested/netcatscan.sh
+++ untested/netcatscan.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-if [ -z "$(which netcat)" ]; then
-	echo "Netcat is not installed."
-  echo "Exiting..."
+if [ "$(pwd)" = "$HOME/Desktop" ]; then
+	if [ -z "$(which netcat)" ]; then
+		echo "Netcat is not installed."
+	  echo "Exiting..."
+		exit 0 
+	else
+	  mkdir netcat 
+	  ps aux | grep nc >> netcat/ncinstances.txt # shows all netcat instances
+		netstat -tuln | grep nc >> netcat/nclistening.txt # shows all ports that netcat is using 
+	  find / -name "*nc*" -type f >> netcat/potentialncfiles.txt # shows all files with NC 
+	fi
+	echo "Created netcat directory and possible netcat files."
+	echo "Remember to also check crontabs to ensure netcat does not run during startup"
+	echo "also remember that netcat should be removed after backdoors are removed"
 	exit 0 
 else
-  mkdir netcat 
-  ps aux | grep nc >> netcat/ncinstances.txt # shows all netcat instances
-	netstat -tuln | grep nc >> netcat/nclistening.txt # shows all ports that netcat is using 
-  find / -name "*nc*" -type f >> netcat/potentialncfiles.txt # shows all files with nc 
-fi
-echo "Created netcat directory and possible netcat files."
-echo "Remember to also check crontabs to ensure netcat does not run during startup"
-echo "also remember that netcat should be removed after backdoors are removed"
+echo "This script should be run in the Desktop directory."
 exit 0 
-
