e867bb1eab0378293f0b7491e6ab319efe3a77d1
usernamegth <112678550+usernamegth@users.noreply.github.com>
2023-10-08 15:31:25 +1100

    Update passwordexpiry.sh

--- untested/passwordexpiry.sh
+++ untested/passwordexpiry.sh
@@ -5,7 +5,12 @@ if [ "$(pwd)" = "$HOME/Desktop" ] && [ -e "users.txt" ]; then
 	chage -m 3 $i
 	passwd -x30 -n3 -w7 $i 
  	usermod -U $i # unlocks the account
-
+  	echo "User $i configured."
+  done
+sed -i '/PASS_MAX_DAYS/s/[0-9]\+/30/' /etc/login.defs
+sed -i '/PASS_MIN_DAYS/s/[0-9]\+/3/' /etc/login.defs
+sed -i '/PASS_WARN_AGE/s/[0-9]\+/7/' /etc/login.defs
+exit 0 
 else 
   echo "This script must be run in the Desktop directory and/or with users.txt."
   echo "Exiting..."
