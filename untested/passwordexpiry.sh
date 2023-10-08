97039320c7125bd263a41be2ece798eb18e386cc
usernamegth <112678550+usernamegth@users.noreply.github.com>
2023-10-08 16:03:00 +1100

    Update passwordexpiry.sh

--- untested/passwordexpiry.sh
+++ untested/passwordexpiry.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-if 
+if [ "$EUID" -ne 0 ]; then
+	echo "This script must be run as root."
+ 	exit 0
+fi 
 if [ "$(pwd)" = "$HOME/Desktop" ] && [ -e "users.txt" ]; then
   for i in $(cat users.txt); do
   	echo -e "CyberTaipan123!\nCyberTaipan123!" | passwd $i
@@ -9,11 +12,12 @@ if [ "$(pwd)" = "$HOME/Desktop" ] && [ -e "users.txt" ]; then
  	usermod -U $i # unlocks the account
   	echo "User $i configured."
   done
-cp /etc/login.defs /etc/backuplogin.defs
-sed -i '/PASS_MAX_DAYS/s/[0-9]\+/30/' /etc/login.defs
-sed -i '/PASS_MIN_DAYS/s/[0-9]\+/3/' /etc/login.defs
-sed -i '/PASS_WARN_AGE/s/[0-9]\+/7/' /etc/login.defs
-exit 0 
+  cp /etc/login.defs /etc/backuplogin.defs
+  sed -i '/PASS_MAX_DAYS/s/[0-9]\+/30/' /etc/login.defs
+  sed -i '/PASS_MIN_DAYS/s/[0-9]\+/3/' /etc/login.defs
+  sed -i '/PASS_WARN_AGE/s/[0-9]\+/7/' /etc/login.defs
+  exit 0
+fi 
 else 
   echo "This script must be run in the Desktop directory and/or with users.txt."
   echo "Exiting..."
