c1fe4a5be1a4a572a57a1aa126b5aa0dd72f4409
usernamegth <112678550+usernamegth@users.noreply.github.com>
2023-10-08 15:11:48 +1100

    Update listpackages.sh

--- untested/listpackages.sh
+++ untested/listpackages.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-apt list --installed >> installedpackages.txt
-# lists all the installed packages
-release=$(lsb_release -sc)
-curl -s "https://old-releases.ubuntu.com/releases/$release/ubuntu-$release-desktop-amd64.manifest" | cut -f1 | cut -d: -f1 | sort -u >> manualpackages.txt 
-# lists all the packages that are manually installed (not originally part of the system)
+if [ "$(pwd)" != "$HOME/Desktop" ]; then
+  echo "This script must be run in the desktop directory."
+  exit 0 
+else
+  apt list --installed >> installedpackages.txt
+  # lists all the installed packages
+  release=$(lsb_release -sc)
+  curl -s "https://old-releases.ubuntu.com/releases/$release/ubuntu-$release-desktop-amd64.manifest" | cut -f1 | cut -d: -f1 | sort -u >> manualpackages.txt 
+  # lists all the packages that are manually installed (not originally part of the system)
+fi 
