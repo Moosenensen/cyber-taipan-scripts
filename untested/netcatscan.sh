34c015945d60bd0ae61864e986b278bcde22326e
usernamegth <112678550+usernamegth@users.noreply.github.com>
2023-10-08 15:06:13 +1100

    Create netcatscan.sh

---
+++ untested/netcatscan.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ -z "$(which netcat)" ]; then
+	echo "Netcat is not installed."
+  echo "Exiting..."
+	exit 0 
+else
+  mkdir netcat 
+  ps aux | grep nc >> netcat/ncinstances.txt # shows all netcat instances
+	netstat -tuln | grep nc >> netcat/nclistening.txt # shows all ports that netcat is using 
+  find / -name "*nc*" -type f >> netcat/potentialncfiles.txt # shows all files with nc 
+fi
+echo "Created netcat directory and possible netcat files."
+echo "Remember to also check crontabs to ensure netcat does not run during startup"
+echo "also remember that netcat should be removed after backdoors are removed"
+exit 0 
+
