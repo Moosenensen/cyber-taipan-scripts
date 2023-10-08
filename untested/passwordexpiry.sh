7163eceb34a74764f9639ab43b5bd78ee2477954
usernamegth <112678550+usernamegth@users.noreply.github.com>
2023-10-08 15:39:40 +1100

    Update passwordexpiry.sh

--- untested/passwordexpiry.sh
+++ untested/passwordexpiry.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 if [ "$(pwd)" = "$HOME/Desktop" ] && [ -e "users.txt" ]; then
-  for i in users.txt; do
+  for i in $(cat users.txt); do
 	chage -M 30 $i
 	chage -m 3 $i
 	passwd -x30 -n3 -w7 $i 
  	usermod -U $i # unlocks the account
   	echo "User $i configured."
   done
+cp /etc/login.defs /etc/backuplogin.defs
 sed -i '/PASS_MAX_DAYS/s/[0-9]\+/30/' /etc/login.defs
 sed -i '/PASS_MIN_DAYS/s/[0-9]\+/3/' /etc/login.defs
 sed -i '/PASS_WARN_AGE/s/[0-9]\+/7/' /etc/login.defs
