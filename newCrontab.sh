f1d76102a430e847a922f5f44488b79462f6ebe6
Riley <111324867+The3agle@users.noreply.github.com>
2023-10-11 17:01:00 +1030

    Update newCrontab.sh with working code

--- newCrontab.sh
+++ newCrontab.sh
@@ -1,10 +1,9 @@
+#!/bin/bash
 for user in $(getent passwd | cut -f1 -d: )
 do
 
-crontabresults=crontab -u $user -l
-if [[ $crontabresults =! "no crontab for $user"]]
 echo $user
-echo $crontabresults
-echo ''
+echo sudo crontab -u $user -l
+echo ' '
+
 done
-echo 'Cron results displayed'
