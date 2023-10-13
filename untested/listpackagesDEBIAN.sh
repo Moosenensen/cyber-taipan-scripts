8dc42d41bbbb71d18a1989e639d01efd3d44bded
usernamegth <112678550+usernamegth@users.noreply.github.com>
2023-10-14 09:32:22 +1100

    Create listpackagesDEBIAN.sh

---
+++ untested/listpackagesDEBIAN.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ "$(pwd)" != "$HOME/Desktop" ]; then
+  echo "This script must be run in the desktop directory."
+  exit 1
+fi
+
+dpkg -l | awk '/ii/ {print $2}' >> installedpackages.txt
+# Lists all the installed packages
+
+dpkg --get-selections | grep -E 'install$' | cut -f1 >> manualpackages.txt
+# Lists all the packages that are manually installed (not originally part of the system)
