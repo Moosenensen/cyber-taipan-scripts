88165b81a1e567f6f1e7066c3f6238f718b7cfd5
usernamegth <112678550+usernamegth@users.noreply.github.com>
2023-10-14 09:35:15 +1100

    Update passwordexpiry.sh

--- untested/passwordexpiry.sh
+++ untested/passwordexpiry.sh
@@ -17,8 +17,9 @@ if [ "$(pwd)" = "$HOME/Desktop" ] && [ -e "users.txt" ]; then
   sed -i '/PASS_MIN_DAYS/s/[0-9]\+/3/' /etc/login.defs
   sed -i '/PASS_WARN_AGE/s/[0-9]\+/7/' /etc/login.defs
   exit 0
-fi 
 else 
   echo "This script must be run in the Desktop directory and/or with users.txt."
   echo "Exiting..."
+fi
 exit 0 
+
